08effc465a4184615e7c460507a36cc3d093395c
izanamiah <jeremiahliu@live.com>
2023-05-12 12:09:57 -0400

    added register modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
-import Modal from "./components/modals/Modal";
+import RegisterModal from "./components/modals/RegisterModal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import { Nunito } from "next/font/google";
+import ToasterProvider from "./providers/ToasterProvider";
 
 export const metadata = {
   title: "Airbnb",
@@ -20,7 +21,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Modal actionLabel="Submit" title="hello world" isOpen />
+        <ToasterProvider />
+        <RegisterModal />
         <Navbar />
         {children}
       </body>
